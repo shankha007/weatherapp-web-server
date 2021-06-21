7eaafd6e750c983c8b3ce6593a408756cb036357
Shankha <ribhumaster@gmail.com>
2021-06-22 01:14:33 +0530

    implemented server for weather app

---
+++ src/app.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+
+// Define paths for Express Config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Shankha'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Shankha'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Shankha',
+        helpMsg: 'This is a help message.'
+    });
+});
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'Sunny',
+        location: 'Kolkata, India'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000!');
+});
